3064731fd18a43d650571d0d3656fbc2e0402d99
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-11 20:11:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toUpperCase) {
+  if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
-  else if (string === string.toLowerCase) {
+  else if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
   else if (string === "I love you, Grandma.") {
